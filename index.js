0d4ec40c092262cbabf0e12b6dc403c4dd96511f
Edward <73508968+edwardhu@users.noreply.github.com>
2021-03-05 11:18:08 +0800

    Add files via upload
    First commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import moment from 'moment';
+// const test = moment("/[0-9]*['a-z\u00A0-\u05FF\u0700-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+|[\u0600-\u06FF\/]+(\s*?[\u0600-\u06FF]+){1,2}/i");
+const test = moment().parse("/[0-9]*['a-z\u00A0-\u05FF\u0700-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+|[\u0600-\u06FF\/]+(\s*?[\u0600-\u06FF]+){1,2}/i");
+console.log(test)
